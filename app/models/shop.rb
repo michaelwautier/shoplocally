93acf30f9109586e1ce0e7913e0ab20d16d0b2f1
Michael Wautier <wautiermichael@gmail.com>
2020-11-16 17:34:52 +0100

    shop mvc

---
+++ app/models/shop.rb
@@ -0,0 +1,8 @@
+class Shop < ApplicationRecord
+  belongs_to :address
+  belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :vat_number, format: { with: /BE\d{10}/ }
+end
