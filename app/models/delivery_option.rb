f15c8b19f1aaa79eceb2843593a5bfc9a4016a65
Karel Asselman <asselman.karel@gmail.com>
2020-11-16 21:14:30 +0100

    deliveryOptions model

--- app/models/delivery_option.rb
+++ app/models/delivery_option.rb
@@ -1,2 +1,5 @@
 class DeliveryOption < ApplicationRecord
+  validates :free_delivery_from, numericality: true
+  validates :delivery_cost, numericality: true
+  validates :express_delivery_cost, numericality: true
 end
