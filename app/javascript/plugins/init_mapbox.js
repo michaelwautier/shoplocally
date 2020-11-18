2ac496ff11deddd7dce60f15d95ffa3e35140dfc
Michael Wautier <wautiermichael@gmail.com>
2020-11-18 18:44:30 +0100

    change zoom duration

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,7 +6,7 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 15000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 4000 });
 };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
