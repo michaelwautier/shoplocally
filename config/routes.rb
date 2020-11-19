bd38298e436c37d55c5b5e2b8bb3ca92830bec5b
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-19 11:56:51 +0100

    Orders controller created, basic view implemented

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :products, except: [:destroy, :update]
     resources :delivery_options, only: [:index, :new, :create, :edit]
   end
+  resources :orders, only: [:index, :show]
 
   # shop independent product actions
   post 'products/:id', to: 'products#add_to_cart', as: 'add_to_cart'
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   get 'cart/', to: 'carts#show', as: 'current_cart'
   get 'cart/checkout/', to: 'carts#checkout', as: 'cart_checkout'
   post 'cart/checkout/', to: 'carts#checkout_confirmation', as: 'cart_checkout_confimation'
-  
+
   # shop independent delivery_option actions
   resources :delivery_options, only: [:destroy, :update]
 
