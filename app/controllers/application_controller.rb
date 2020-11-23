2a1a77ff43b7b215198a2bab93398159b60250cc
Michael Wautier <wautiermichael@gmail.com>
2020-11-23 10:53:32 +0100

    ui consistency on overall app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar address])
   end
 
   def default_url_options
