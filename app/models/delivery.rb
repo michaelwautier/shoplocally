b297d9b89e877ff4e0439b6dcc2e4e752d2ebd08
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-24 20:37:34 +0100

    New status pending_delivery_pickup and pending_customer_pickup

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,5 +1,5 @@
 class Delivery < ApplicationRecord
-  STATUS = %w[pending delivery_pickup customer_pickup assigned in_transport undeliverable delivered]
+  STATUS = %w[pending_delivery_pickup pending_customer_pickup assigned in_transport undeliverable delivered]
   belongs_to :order
   belongs_to :shop
   belongs_to :user, optional: true
