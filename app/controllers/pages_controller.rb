54feb4a71f4799afde327d74329b79cab7fa519a
Michael Wautier <wautiermichael@gmail.com>
2020-11-25 20:38:05 +0100

    new page for search query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, except: %i[home]
-  def home
+  def home; end
+
+  def search
+    sql_query = "name ILIKE :query"
+    @shops_name = Shop.where(sql_query, query: "%#{params[:query]}%")
+    @products = Product.where(sql_query, query: "%#{params[:query]}%")
+    @shops = []
+    @products.each do |product|
+      @shops << product.shop
+    end
+    @shops = @shops.uniq
   end
 end
